d9021e9970e09544be145c9dfdbac5dd06a3fb2c
thyoon515 <thyoon515@gmail.com>
2022-08-22 14:14:57 -0400

    commit tree 05c55be43de1c231c4c7f768fb1b770875b0b43b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind(lengths)
+    lengths.each do |a, b, c|
+      if a == b && a == c
+        :equilateral
+      elsif a == b
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
 end
